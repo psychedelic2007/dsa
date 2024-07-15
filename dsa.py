a97162141b8c600f3c5aedc3eb82bb4d4b6a48a6
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2024-07-15 20:12:13 +1000

    Update dsa.py

--- dsa.py
+++ dsa.py
@@ -7,6 +7,8 @@ from util.pages.equilibrium import equilibrium
 from util.pages.phase import main
 from util.pages.two_phase import two_phase
 from util.pages.bifurcation import bifurcation_page
+from util.pages.two_compartment_sim import sim
+from util.pages.three_compartment_sim import sim_three_compartment
 
 app = MultiApp()
 
@@ -15,6 +17,7 @@ app.add_app("Equilibrium Analysis", equilibrium)
 app.add_app("Phase Plane Analysis", main)
 app.add_app("2C Phase Analysis", two_phase)
 app.add_app("Bifurcation Analysis", bifurcation_page)
-
+app.add_app("Two Compartment Simulation", sim)
+app.add_app("Three Compartment Simulation", sim_three_compartment)
 app.run()
 
